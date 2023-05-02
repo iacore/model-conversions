b85f152b3065b036530d4b60b95004c047411a0a
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-05-02 11:09:18 +0000

    add old scripts

---
+++ torch-to-safetensors.py
@@ -0,0 +1,17 @@
+import sys
+from torch import load as torch_load
+from safetensors.torch import save_file
+
+try:
+    file = sys.argv[1]
+    fout = sys.argv[2]
+except IndexError:
+    print("Usage: THIS_SCRIPT abc.pth abc.safetensors")
+    exit(1)
+
+weights = torch_load(file, "cpu")
+
+for k in weights.keys():
+    print(k, weights[k].size(), weights[k].dtype)
+
+save_file(weights, fout)
